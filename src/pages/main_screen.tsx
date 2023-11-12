1acb61ecdd3795b02f69e4b19c73ed334de63440
wooniekim <dawun3295@gmail.com>
2023-11-12 13:07:09 +0900

    sexond commit

--- src/pages/main_screen.tsx
+++ src/pages/main_screen.tsx
@@ -5,9 +5,14 @@ import { Link } from "react-router-dom";
 const Main = () => {
   return (
     <div>
-      <h1>MAIN</h1>
-      <p>메인스크린입니다.</p>
-      <Link to="/login">Login</Link>
+      <div className="w-full h-full position: fixed">
+        <div className="min-h-screen bg-red-200 flex justify-center items-center">
+          <div className="absolute w-60 h-60 rounded-xl bg-yellow-400 -top-5 -left-16 z-0 transform rotate-45 hidden md:block"></div>
+          <div className="absolute w-48 h-48 rounded-xl bg-yellow-400 -bottom-6 -right-10 transform rotate-12 hidden md:block"></div>
+          <div className="w-40 h-40 absolute bg-purple-300 rounded-full top-0 right-12 hidden md:block"></div>
+          <div className="w-20 h-40 absolute bg-purple-300 rounded-full bottom-20 left-10 transform rotate-45 hidden md:block"></div>
+        </div>
+      </div>
     </div>
   );
 };
